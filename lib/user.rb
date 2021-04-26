93af1f1d8520adcc8c364da76cadedb72a7c4682
Septentrion50 <erwan56lecorre@gmail.com>
2021-04-26 10:33:51 +0200

    Sauvegarde de l'état arrivé au point 3.5

---
+++ lib/user.rb
@@ -0,0 +1,51 @@
+require 'pry'
+
+# a user
+class User
+  attr_writer :mastercard
+  attr_reader :birth_date
+  attr_accessor :email
+  @@user_count = 0
+
+  def initialize(email_to_save)
+    @email = email_to_save
+    puts 'On envoie un email de bienvenue !'
+    @@user_count += 1
+  end
+
+  def self.count
+    return @@user_count
+  end
+
+  def greet
+    puts 'Bonjour, monde !'
+  end
+
+  def say_hello_to_someone(first_name)
+    puts "Bonjour, #{first_name} !"
+  end
+
+  def show_itself
+    print "Voici l'instance : "
+    puts self
+  end
+
+  def update_email(email_to_update)
+    @email = email_to_update
+  end
+
+  def read_email
+    return @email
+  end
+
+  def read_mastercard
+    return @mastercard
+  end
+
+  def update_birthdate(birthdate_to_update)
+    @birth_date = birthdate_to_update
+  end
+end
+
+binding.pry
+puts 'end of file'
