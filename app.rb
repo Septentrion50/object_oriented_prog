0abf62a5e4bf69094a343e0fb7aa422be852ceb3
Septentrion50 <erwan56lecorre@gmail.com>
2021-04-27 01:01:48 +0200

    Ajout de la classe EventCreator dans app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,61 @@
 require_relative './lib/user'
 require_relative './lib/event'
 
+# A nice menu for our calendar
+class EventCreator
+  @@all = []
 
+  def initialize
+    puts "Commençons la création d'un événement."
+    @name_event = name_event
+    @moment = moment
+    @dur = dur
+    @who = who
+    new_event = Event.new(@moment, @dur, @name_event, @who)
+    @@all << new_event
+    puts "L'événement a été créé !"
+  end
+
+  def name_event
+    puts "Quel est le nom de l'évenement ?"
+    print '> '
+    gets.chomp
+  end
+
+  def moment
+    puts "Quand aura t'il lieu ?"
+    print '> '
+    gets.chomp
+  end
+
+  def dur
+    puts "Combien de temps durera t'il ?"
+    print '> '
+    gets.chomp.to_i
+  end
+
+  def who
+    puts 'Qui viendra ?'
+    print '> '
+    gets.chomp.split(';')
+  end
+
+  def self.all_events
+    @@all.each do |i|
+      i.to_s
+      puts
+    end
+  end
+
+  def self.event_by_name(name)
+    @@all.each do |i|
+      if i.title == name
+        i
+      end
+    end
+  end
+end
+
+my_event = EventCreator.new
+
+EventCreator.all_events
